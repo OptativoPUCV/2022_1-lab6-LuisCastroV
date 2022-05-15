985bb972ee45548c962a19974af54dea020d9e00
LuisCastroV <>
2022-05-15 05:36:46 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -176,7 +176,7 @@ Node* DFS(Node* initial, int* cont)
     }
     List* l=get_adj_nodes(n);
     Node* adj=first(l);
-    while(adj!=NULL)
+    while(adj)
     {
       push(S,adj);
       adj=next(l);
