ff945521c5d818bb67684e59aaa6014dc5d00450
LuisCastroV <>
2022-05-15 17:55:24 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -146,7 +146,6 @@ List* get_adj_nodes(Node* n)
   return list;
 }
 
-
 int is_final(Node* n)
 {
   for(int i=0;i<9;i++)
@@ -164,26 +163,26 @@ int is_final(Node* n)
 
 Node* DFS(Node* initial, int* cont)
 {
-  // Stack* S = createStack();
-  // push(S, initial);
-  // while(get_size(S)!=0)
-  // {
-  //   Node* n=top(S); 
-  //   pop(S);
-  //   if(is_final(n))
-  //   { 
-  //     return n;
-  //   }
-  //   List* l=get_adj_nodes(n);
-  //   Node* adj=first(l);
-  //   while(adj)
-  //   {
-  //     push(S,adj);
-  //     adj=next(l);
-  //   }
-  //   free(n);
-  //   cont++;
-  // }  
+  Stack* S = createStack();
+  push(S, initial);
+  while(get_size(S)!=0)
+  {
+    Node* n=top(S); 
+    pop(S);
+    if(is_final(n))
+    { 
+      return n;
+    }
+    List* l=get_adj_nodes(n);
+    Node* adj=first(l);
+    while(adj)
+    {
+      push(S,adj);
+      adj=next(l);
+    }
+    free(n);
+    cont++;
+  }  
   return NULL;
 }
 
