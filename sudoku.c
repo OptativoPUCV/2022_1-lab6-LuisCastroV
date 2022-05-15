30d3eb4adb09598236d277d4538a5b3e9aa1a2e6
LuisCastroV <>
2022-05-15 04:30:25 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -53,9 +53,13 @@ List* get_adj_nodes(Node* n)
 {
   List* list=createList(); 
   for(int i=0;i<9;i++)
-      {
-        
-      }
+  {
+    for(int j=0;j<9;j++)
+    {
+      Node* Adj=copy(n);
+      Adj->sudo[i][j];
+    }
+  }
   return list;
 }
 
